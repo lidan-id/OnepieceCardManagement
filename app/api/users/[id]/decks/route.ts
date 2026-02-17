154189a8bab97b1dd1de3c480814d3879144f2b5
Kevin Lidan <lidannn012@gmail.com>
2026-02-17 18:00:02 +0700

    Fix params type for Next.js 16

--- app/api/users/[id]/decks/route.ts
+++ app/api/users/[id]/decks/route.ts
@@ -2,8 +2,9 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } },
+  props: { params: Promise<{ id: string }> },
 ) {
+  const params = await props.params;
   const userId = params.id;
 
   const userDecks = await prisma.deck.findMany({ where: { userId: userId } });
